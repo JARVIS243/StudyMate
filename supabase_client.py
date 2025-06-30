f2cde15fa57fc0428e5e02cce6115877c98b2414
JARVIS243 <jarvisai0025@gmail.com>
2025-06-30 17:19:04 +0530

    supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -4,8 +4,8 @@ from supabase import create_client
 from dotenv import load_dotenv
 
 load_dotenv()
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
+url = os.environ("SUPABASE_URL")
+key = os.environ("SUPABASE_KEY")
 supabase = create_client(url, key)
 
 # Register new user
